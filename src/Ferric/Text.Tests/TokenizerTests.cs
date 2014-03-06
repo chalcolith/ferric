0dd2966cb3a6ddb63a4492837e25bc709a64d5c5
Gordon Tisher <gordon@balafon.net>
2014-03-05 16:32:34 -0800

    move unicode category tokenizer from Lang.English to Text

---
+++ src/Ferric/Text.Tests/TokenizerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Ferric.Text.Tokenizer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ferric.Text.Tests
+{
+    [TestClass]
+    public class TokenizerTests
+    {
+        [TestMethod]
+        public void Text_Tokenizer_TestUnicodeRegexpTokenizer()
+        {
+            var input = @"abc123.$ i'll";
+            var tokens = new UnicodeRegexpTokenizer().Process(input).OfType<UnicodeTokenSpan>().ToArray();
+
+            Assert.AreEqual(UnicodeTokenClass.Word, tokens[0].TokenClass);
+            Assert.AreEqual("abc", tokens[0].Text);
+
+            Assert.AreEqual(UnicodeTokenClass.Number, tokens[1].TokenClass);
+            Assert.AreEqual("123", tokens[1].Text);
+
+            Assert.AreEqual(UnicodeTokenClass.Punct, tokens[2].TokenClass);
+            Assert.AreEqual(".", tokens[2].Text);
+
+            Assert.AreEqual(UnicodeTokenClass.Symbol, tokens[3].TokenClass);
+            Assert.AreEqual("$", tokens[3].Text);
+
+            Assert.AreEqual(UnicodeTokenClass.Space, tokens[4].TokenClass);
+            Assert.AreEqual(" ", tokens[4].Text);
+
+            Assert.AreEqual(UnicodeTokenClass.Word, tokens[5].TokenClass);
+            Assert.AreEqual("i", tokens[5].Text);
+
+            Assert.AreEqual(UnicodeTokenClass.Punct, tokens[6].TokenClass);
+            Assert.AreEqual("'", tokens[6].Text);
+
+            Assert.AreEqual(UnicodeTokenClass.Word, tokens[7].TokenClass);
+            Assert.AreEqual("ll", tokens[7].Text);
+        }
+    }
+}
---
+++ src/Ferric/Text/Tokenizer/UnicodeRegexpTokenizer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferric.Text.Tokenizer
+{
+    public enum UnicodeTokenClass
+    {
+        Word,
+        Number,
+        Punct,
+        Symbol,
+        Space,
+        Other
+    }
+
+    public class UnicodeTokenSpan : Ferric.Text.Tokenizer.TokenSpan
+    {
+        public UnicodeTokenClass TokenClass { get; internal set; }
+
+        public UnicodeTokenSpan(UnicodeTokenClass tokenClass, string text, ulong charPos, ulong charNext, ulong ordinal)
+            : base()
+        {
+            this.TokenClass = tokenClass;
+            this.Text = text;
+            this.CharPos = charPos;
+            this.CharNext = charNext;
+            this.Ordinal = ordinal;
+        }
+    }
+
+    public class UnicodeTokenRegexp : TokenRegexp
+    {
+        public UnicodeTokenClass TokenClass { get; internal set; }
+
+        public UnicodeTokenRegexp(string regexp, UnicodeTokenClass tokenClass)
+            : base(regexp)
+        {
+            this.TokenClass = tokenClass;
+        }
+
+        public override TokenSpan OnReadToken(string text, ulong charPos, ulong charNext, ulong ordinal)
+        {
+            return new UnicodeTokenSpan(TokenClass, text, charPos, charNext, ordinal);
+        }
+    }
+
+    public class UnicodeRegexpTokenizer : RegexpTokenizer
+    {
+        static readonly IList<TokenRegexp> defaultTokens = new List<TokenRegexp>
+        {
+            new UnicodeTokenRegexp(@"[\p{Lu}\p{Ll}\p{Lt}\p{Lm}\p{Lo}\p{Mn}\p{Mc}\p{Me}]+", UnicodeTokenClass.Word),
+            new UnicodeTokenRegexp(@"[\p{Nd}\p{Nl}\p{No}]+", UnicodeTokenClass.Number),
+            new UnicodeTokenRegexp(@"[\p{Pc}\p{Pd}\p{Ps}\p{Pe}\p{Pi}\p{Pf}\p{Po}]", UnicodeTokenClass.Punct),
+            new UnicodeTokenRegexp(@"[\p{Sm}\p{Sc}\p{Sk}\p{So}]", UnicodeTokenClass.Symbol),
+            new UnicodeTokenRegexp(@"[\p{Zs}\p{Zl}\p{Zp}]+", UnicodeTokenClass.Space),
+            new UnicodeTokenRegexp(@"[\p{Cc}\p{Cf}\p{Cs}\p{Co}\p{Cn}]", UnicodeTokenClass.Other)
+        };
+
+        public UnicodeRegexpTokenizer()
+            : this(defaultTokens)
+        {
+        }
+
+        public UnicodeRegexpTokenizer(IList<TokenRegexp> tokens)
+            : base(tokens)
+        {
+        }
+    }
+}
