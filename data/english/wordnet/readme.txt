c1e8986772db3dcb08ac9bde12dab288cd12120b
Gordon Tisher <gordon@balafon.net>
2014-04-25 15:00:11 -0700

    add stub for wordnet project

---
+++ data/english/wordnet/readme.txt
@@ -0,0 +1,5 @@
+You need to download the SqLite WordNet database from 
+
+  http://downloads.sourceforge.net/project/wnsql/wnsql3/sqlite/3.1/sqlite-31.db.zip
+
+and then extract the file sqlite-31.db to this directory.
---
+++ src/Ferric/Text/WordNet.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ferric.Text.WordNet.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
